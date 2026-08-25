bed1948c12a29ab45ad4d64e2e0488d576fc2674
palakash26 <palakash26@users.noreply.github.com>
2026-08-25 16:42:47 +0530

    \"Update_middleware_to_clerkMiddleware\"

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,27 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// import { authMiddleware } from "@clerk/nextjs/server";
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/ping(.*)",
+  "/ping(.*)",
+  "/book-room/(.*)",
+]);
 
-export default authMiddleware({
-  publicRoutes: [
-    "/((?!api|trpc))(_next.*|.+\\.[\\w]+$)", // Default Clerk ignored routes
-    "/", 
-    "/user/bookings",
-    "/api/ping",
-    "/ping",
-  ],
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    auth().protect();
+  }
 });
+
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
+
 
 
 
